d5c9a21d2c777fb1699ca887e8dc2df0fea4fb3d
Q.Y <qin.yu.95@outlook.com>
2016-03-08 22:35:15 +0000

    Initial frame
    Let work together!

---
+++ www/js/controllers.js
@@ -0,0 +1,54 @@
+angular.module('app.controllers', [])
+  
+.controller('uCLGambleCtrl', function($scope) {
+
+})
+   
+.controller('profileCtrl', function($scope) {
+
+})
+   
+.controller('settingCtrl', function($scope) {
+
+})
+      
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('trustGameCtrl', function($scope) {
+
+})
+   
+.controller('publicGoodsCtrl', function($scope) {
+
+})
+   
+.controller('ultimatumGameCtrl', function($scope) {
+
+})
+   
+.controller('probabilityGameCtrl', function($scope) {
+
+})
+   
+.controller('beautyContestGameCtrl', function($scope) {
+
+})
+   
+.controller('timeEstimationGameCtrl', function($scope) {
+
+})
+   
+.controller('rockGuessingGameCtrl', function($scope) {
+
+})
+   
+.controller('aboutUsCtrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,162 @@
+angular.module('app.routes', ['ionicUIRouter'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.uCLGamble', {
+    url: '/page2',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/uCLGamble.html',
+        controller: 'uCLGambleCtrl'
+      }
+    }
+  })
+
+  /* 
+    The IonicUIRouter.js UI-Router Modification is being used for this route.
+    To navigate to this route, do NOT use a URL. Instead use one of the following:
+      1) Using the ui-sref HTML attribute:
+        ui-sref='tabsController.profile'
+      2) Using $state.go programatically:
+        $state.go('tabsController.profile');
+    This allows your app to figure out which Tab to open this page in on the fly.
+    If you're setting a Tabs default page or modifying the .otherwise for your app and
+    must use a URL, use one of the following:
+      /page1/tab1/page3
+      /page1/tab3/page3
+  */
+  .state('tabsController.profile', {
+    url: '/page3',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/profile.html',
+        controller: 'profileCtrl'
+      },
+      'tab3': {
+        templateUrl: 'templates/profile.html',
+        controller: 'profileCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.setting', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/setting.html',
+        controller: 'settingCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('signup', {
+    url: '/page5',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('login', {
+    url: '/page6',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('tabsController.trustGame', {
+    url: '/page7',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/trustGame.html',
+        controller: 'trustGameCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.publicGoods', {
+    url: '/page8',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/publicGoods.html',
+        controller: 'publicGoodsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.ultimatumGame', {
+    url: '/page9',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/ultimatumGame.html',
+        controller: 'ultimatumGameCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.probabilityGame', {
+    url: '/page10',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/probabilityGame.html',
+        controller: 'probabilityGameCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.beautyContestGame', {
+    url: '/page11',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/beautyContestGame.html',
+        controller: 'beautyContestGameCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.timeEstimationGame', {
+    url: '/page12',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/timeEstimationGame.html',
+        controller: 'timeEstimationGameCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.rockGuessingGame', {
+    url: '/page13',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/rockGuessingGame.html',
+        controller: 'rockGuessingGameCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.aboutUs', {
+    url: '/page14',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/aboutUs.html',
+        controller: 'aboutUsCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/page1/page2')
+
+  
+
+});
