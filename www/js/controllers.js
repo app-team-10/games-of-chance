704f1efbb206efd38fd3eb239e23061739f25540
Q.Y <qin.yu.95@outlook.com>
2016-03-14 01:34:10 +0000

    Basic Registration Done
    One can register and I can see it in my database. The registration form
    has validation check.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,8 +12,26 @@ angular.module('app.controllers', [])
 
 })
       
-.controller('signupCtrl', function($scope) {
+.controller('signupCtrl', function($scope, $firebaseAuth, FIREBASE_URL) {
+    $scope.message = "nothing!!";
+    $scope.user = {};
+    var ref = new Firebase(FIREBASE_URL);
+    var auth = $firebaseAuth(ref);
+    
+    $scope.login = function() {
+      $scope.message = "Welcome " + $scope.user.email;
+    };
 
+    $scope.register = function() {
+        auth.$createUser({
+            email: $scope.user.email,
+            password: $scope.user.password
+        }).then(function(regUser) {
+            $scope.message = "Hi " + $scope.user.firstname + ", Thanks for registering";
+        }).catch(function(error) {
+            $scope.message = error.message;
+        });
+    };
 })
    
 .controller('loginCtrl', function($scope) {
