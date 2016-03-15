3f50864d045d565b079e61deeeb634864beaf5a8
Q.Y <qin.yu.95@outlook.com>
2016-03-15 12:38:53 +0000

    Success in Extraction
    created another controller js file for registration and  login.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['registration'])
   
 .controller('uCLGambleCtrl', function($scope) {
 
@@ -10,28 +10,6 @@ angular.module('app.controllers', [])
    
 .controller('settingCtrl', function($scope) {
 
-})
-      
-.controller('signupCtrl', function($scope, $firebaseAuth, FIREBASE_URL) {
-    $scope.message = "nothing!!";
-    $scope.user = {};
-    var ref = new Firebase(FIREBASE_URL);
-    var auth = $firebaseAuth(ref);
-    
-    $scope.login = function() {
-      $scope.message = "Welcome " + $scope.user.email;
-    };
-
-    $scope.register = function() {
-        auth.$createUser({
-            email: $scope.user.email,
-            password: $scope.user.password
-        }).then(function(regUser) {
-            $scope.message = "Hi " + $scope.user.firstname + ", Thanks for registering";
-        }).catch(function(error) {
-            $scope.message = error.message;
-        });
-    };
 })
    
 .controller('loginCtrl', function($scope) {
