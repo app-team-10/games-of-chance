acd3d3533fd81d4000b33e86553db5573add90b2
Q.Y <qin.yu.95@outlook.com>
2016-03-23 22:44:34 +0000

    Basic Probability Game Integrated (using CONNOR's functions)
    A functioning version. Not completed. (Controllers are modified because
    of the dependency issue.)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', ['registration', 'app.services', 'publicpool', 'publicpoolCheck', 'selection'])
+angular.module('app.controllers', ['probability', 'registration', 'app.services', 'publicpool', 'publicpoolCheck', 'selection'])
 // Just found out the order of dependency matters: app.service before registration will cause currentUser undefined.
 
 /**
@@ -45,10 +45,6 @@ angular.module('app.controllers', ['registration', 'app.services', 'publicpool',
 })
    
 .controller('ultimatumGameCtrl', function($scope) {
-
-})
-   
-.controller('probabilityGameCtrl', function($scope) {
 
 })
    
